e2f84938fc4a56aeaf2ec614b06c07a8684a55c7
Antoine Toubhans <antoine@toubhans.org>
2017-09-09 21:07:03 +0200

    Remove old implementaion files

--- lib/connector_raw.py
+++
@@ -1,7 +0,0 @@
-from lib.connector_base import Connector as Base_connector
-
-class Connector(Base_connector):
-    def push(self, document):
-        result = self.get_collection('raw').insert_one(document)
-
-        return 1 if result.acknowledged else 0
--- lib/utils.py
+++
@@ -1,2 +0,0 @@
-def is_name_valid(name):
-    return str == type(name) and not name.startswith('_')
