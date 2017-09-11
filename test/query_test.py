4c409f2354ebd6a9ac17b123c1fe8e249d9b7d5c
Antoine Toubhans <antoine@toubhans.org>
2017-09-11 21:34:44 +0200

    Add _get_keys_from_interval method

--- test/query_test.py
+++ test/query_test.py
@@ -84,3 +84,27 @@ class MongoTSQueryBuilderTest(unittest.TestCase):
                 self.assertEqual(day['sum'], 0)
                 self.assertEqual(day['sum2'], 0)
                 self.assertEqual(len(day['hours']), 24)
+
+    def valid_intervals():
+        return [
+            ('1y', []),
+            ('year', []),
+            ('1m', ['months']),
+            ('month', ['months']),
+            ('1d', ['months', 'days']),
+            ('day', ['months', 'days']),
+            ('1h', ['months', 'days', 'hours']),
+            ('hour', ['months', 'days', 'hours']),
+        ]
+
+    @data_provider(valid_intervals)
+    def test_get_keys_from_interval_returns_correct_keys(self, interval, keys):
+        self.assertEqual(query._get_keys_from_interval(interval), keys)
+
+    def invalid_intervals():
+        return [('t'), ('d'), ('m'), ('1')]
+
+    @data_provider(invalid_intervals)
+    def test_get_keys_from_interval_fails_for_invalid_interval(self, invalid_interval):
+        with self.assertRaises(Exception):
+            query._get_keys_from_interval(invalid_interval)
