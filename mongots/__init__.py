2ecc1bc3b33c64498615afdedb0f92283c495d01
Antoine Toubhans <antoine@toubhans.org>
2018-01-26 15:28:50 +0100

    Bump version: 0.1.3 → 0.1.4

--- mongots/__init__.py
+++ mongots/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from mongots.client import MongoTSClient
 from mongots.database import MongoTSDatabase
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ description = 'A fast API for storing and querying time series in MongoDb'
 setup(
     name='mongots',
     packages=find_packages(include=['mongots']),
-    version='0.1.3',
+    version='0.1.4',
     description=description,
     long_description=description,
     author='Antoine Toubhans',
